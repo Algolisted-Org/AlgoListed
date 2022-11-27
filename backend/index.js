2399384a6419dcf4375441468ac413cbc0472b39
Nayaker <nayak.primary@gmail.com>
2022-11-28 01:52:56 +0530

    cors error resolved mark1

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const morgan = require('morgan');
 require('dotenv').config({
   path: './config.env',
 });
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 //Middlewares
 app.use(express.urlencoded({ extended: true }));
