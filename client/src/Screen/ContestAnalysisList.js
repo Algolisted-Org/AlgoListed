3076decf4420cbef39328509d9ab7a539984c08e
Nayaker <nayak.primary@gmail.com>
2023-10-06 07:48:19 +0530

    Dark mode toggle added

--- client/src/Screen/ContestAnalysisList.js
+++ client/src/Screen/ContestAnalysisList.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import CCHeader from '../Components/CCHeader';
+import styled, { css } from 'styled-components';
+import CCHeaderPlus from '../Components/CCHeaderPlus';
+import CCHeaderDark from '../Components/CCHeaderDark';
 import LeftMenu from '../Components/LeftMenu';
+import LeftMenuDark from '../Components/LeftMenuDark';
 import axios from 'axios';
 import { contestAnalysisFilters } from '../Components/contestAnalysisFilters';
 import LockIcon from '@material-ui/icons/Lock';
 import WarningIcon from '@material-ui/icons/Warning';
 import SearchIcon from '@material-ui/icons/Search';
+import CCHeaderDarkPlus from '../Components/CCHeaderDarkPlus';
+
 
 
 const ContestAnalysisList = () => {
   const [platformName, setPlatformName] = useState('leetcode');
   const [contestType, setContestType] = useState('Weekly Contest');
   const [contestNumber, setContestNumber] = useState('361');
+  const [needDarkMode, setNeedDarkMode] = useState(false);
+
+  console.log("needDarkMode : ", needDarkMode);
+  const toggleDarkMode = () => {
+    setNeedDarkMode(!needDarkMode);
+  };
 
   const filters = contestAnalysisFilters.map((item) => {
     return (
@@ -42,9 +52,14 @@ const ContestAnalysisList = () => {
         width more than 1100px, a standard laptop or tablet landscape.
         <img src="https://media4.giphy.com/media/13FrpeVH09Zrb2/giphy.gif" alt="" />
       </MobContainer>
-      <Container>
-        <CCHeader />
-        <LeftMenu marked={"contest-analysis"} />
+      <Container needDarkMode={needDarkMode}>
+        {
+          needDarkMode ? <CCHeaderDarkPlus needDarkMode={needDarkMode} toggleDarkMode={toggleDarkMode}/> : <CCHeaderPlus needDarkMode={needDarkMode} toggleDarkMode={toggleDarkMode}/>
+        }
+        
+        {
+          needDarkMode ? <LeftMenuDark marked={"contest-analysis"} /> : <LeftMenu marked={"contest-analysis"} />
+        }
         <div className="cc-middle-content">
           <h1 className='main-heading'>Contest Analysis</h1>
           <p className="heading-supporter">
@@ -56,9 +71,9 @@ const ContestAnalysisList = () => {
               A particular feature you have in mind that you'd like to see implemented on this page? <a href="https://github.com/Nayaker/AlgoListed/issues/new">create an enhancement issue</a>
             </div>
           </div>
-          <Filters>{filters}</Filters>
+          <Filters needDarkMode={needDarkMode}>{filters}</Filters>
           <CleanLine />
-          <Filters2>
+          <Filters2 needDarkMode={needDarkMode}>
             <div className="filter selected">Contests Analysis</div>
             <div className="filter">Contests Archive</div>
           </Filters2>
@@ -134,8 +149,14 @@ const Container = styled.div`
     justify-content: space-between;
     padding-left: 200px;
 
+    background-color: ${(props) => (props.needDarkMode ? '#313338' : 'transparent')};
+
     a{
-      color: #18489f;
+      color: ${(props) => (props.needDarkMode ? '#6d93d8' : '#18489f')};
+    }
+
+    input{
+      background-color: transparent;
     }
 
     .cc-middle-content{
@@ -157,17 +178,17 @@ const Container = styled.div`
       .main-heading{
           font-size: 1.65rem;
           font-weight: 600;
-          color: #292929;
+          color: ${(props) => (props.needDarkMode ? '#e5e6e8' : '#292929')};
       }
 
       .heading-supporter{
           font-size: 1.05rem;
           margin-bottom: 10px;
           font-weight: 400;
-          color: #696168;
-
+          color: ${(props) => (props.needDarkMode ? '#ffffffa6' : '#696168')};
+          
           a{
-            color: #18489f;
+            color: ${(props) => (props.needDarkMode ? '#18489f' : '#18489f')};
             font-size: 0.95rem;
             font-weight: 300;
             margin-left: 0.25rem;
@@ -178,18 +199,24 @@ const Container = styled.div`
         display: inline-block;
         /* display: flex; */
         /* align-items: center; */
-        background-color: #d5f7e1;
+        background-color: ${(props) => (props.needDarkMode ? '#444754' : '#d5f7e1')};
         border-radius: 5px;
         padding: 10px;
         margin: 20px 0 10px 0;
 
         .text{
             font-size: 0.8rem;
-            color: #13803b;
+            color: ${(props) => (props.needDarkMode ? '#b7b8ba' : '#13803b')};
             font-weight: 300;
             
+            b{
+                font-weight: 500;
+                color: ${(props) => (props.needDarkMode ? '#b7b8ba' : '#13803b')};
+            }
         }
       }
+
+      
       
       .message2{
         display: flex;
@@ -243,6 +270,7 @@ const Container = styled.div`
             border: none;
             font-weight: 400;
             font-size: 0.85rem;
+		        color: ${(props) => (props.needDarkMode ? '#e5e5e5' : '#333')};
           }
         }
         
@@ -254,6 +282,7 @@ const Container = styled.div`
           cursor: pointer;
           display: grid;
           place-items: center;
+          
 
           svg{
             font-size: 1.35rem;
@@ -268,9 +297,13 @@ const Container = styled.div`
       .note{
         font-weight: 200;
         font-size: 0.85rem;
+        color: ${(props) => (props.needDarkMode ? '#e5e5e5' : '#333')};
+
 
         b{
           font-weight: 400;
+          color: ${(props) => (props.needDarkMode ? '#e5e5e5' : '#333')};
+
         }
       }
     }
@@ -290,6 +323,7 @@ const Filters = styled.div`
 		font-weight: 300;
 		text-decoration: none;
 		color: inherit;
+    background-color: ${(props) => (props.needDarkMode ? '#444754' : '#f1f1f1')};
 
     svg{
       font-size: 1rem;
@@ -309,17 +343,19 @@ const Filters = styled.div`
 
   .locked-feature{
     &:hover{
-      background-color: #f1f1f1;
-      color: #333;
+      background-color: ${(props) => (props.needDarkMode ? '#4a4d5a' : '#f2eeee')};
+      color: black;
+      border: 1px solid ${(props) => (props.needDarkMode ? '#fff' : '#7e7676')};
+      transition-duration: 250ms;
     }
   }
 
 	.selected {
 		/* background-color: #ded7d7;
     color: #111; */
-		border-color: #201f1f;
-		background-color: #201f1f;
-		color: #ebdddd;
+    color: ${(props) => (props.needDarkMode ? '#ebdddd' : '#ebdddd')};
+    border: 1px solid ${(props) => (props.needDarkMode ? '#fff' : '#fff')};
+    background-color: ${(props) => (props.needDarkMode ? '#201f1f' : '#201f1f')};
 	}
 
 	@media only screen and (max-width: 1100px) {
@@ -353,12 +389,12 @@ const Filters2 = styled.div`
 		font-size: 0.7rem;
     text-transform: uppercase;
     letter-spacing: 0.07rem;
-		border: 1px solid #b9afaf;
+		border: 1px solid #675f5f;
 		border-radius: 10px;
 		margin: 0px 5px 5px 0px;
 		font-weight: 300;
 		text-decoration: none;
-		color: inherit;
+		color: ${(props) => (props.needDarkMode ? '#e5e5e5' : '#333')};
 
     svg{
       font-size: 1rem;
@@ -371,17 +407,18 @@ const Filters2 = styled.div`
 			transition-duration: 250ms;
 			cursor: pointer;
       border-color: rgb(185, 175, 175);
-      background-color: #e5e5e5;
-      color: #201f1f;
+  		background-color: ${(props) => (props.needDarkMode ? '#404249' : '#e5e5e5')};
+  		border-color: ${(props) => (props.needDarkMode ? '#aea4a4' : 'rgb(185, 175, 175)')};
+  		color: ${(props) => (props.needDarkMode ? '#fff' : '#201f1f')};
 		}
 	}
 
 	.selected {
 		/* background-color: #ded7d7;
     color: #111; */
-		border-color: rgb(185, 175, 175);
-		background-color: #e5e5e5;
-		color: #201f1f;
+		border-color: #675f5f;
+    background-color: ${(props) => (props.needDarkMode ? '#404249' : '#e5e5e5')};
+    color: ${(props) => (props.needDarkMode ? '#e5e5e5' : '#201f1f')};
 	}
 
 
