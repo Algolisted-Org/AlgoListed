3513b3ec8616a7e5c147271573ad62bd9ddd0749
Mohit030802 <mohitom2002@gmail.com>
2023-10-07 20:50:12 +0530

    contributors ui

--- client/src/Screen/ContributorWork.js
+++ client/src/Screen/ContributorWork.js
@@ -1,65 +1,88 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import styled from 'styled-components'
-import data from '../Components/contributorsData.json'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import data from "../Components/contributorsData.json";
 
 const ContributorWork = () => {
-    useEffect(() => {
-        document.title = "Contributor Work | Organisation Information - Algolisted";
-    }, []);
+  useEffect(() => {
+    document.title = "Contributor Work | Organisation Information - Algolisted";
+  }, []);
 
-    const [contributorsPullRequests, setContributorsPullRequests] = useState(data);
+  const [contributorsPullRequests, setContributorsPullRequests] = useState(data);
+  const [isVisible, setIsVisible] = useState([]);
 
-    return (
-        <Container>
-            {/* The below classes are being used from the parent */}
-            <h1 className='main-heading'>Contributor Work</h1>
-            <p className="heading-supporter">
-                This page will contain information about all the people who have contributed, their name, username, first commit, last commit merged. 
-                And the agenda for each PR. 
-            </p>
-            <div className="hold-contributors">
-                {contributorsPullRequests != null &&
-                  contributorsPullRequests.map((item, index) => {
-                    return (
-                      <div key={index} className="contributor-work-container">
-                        <div className="contributor-info">
-                          <a
-                            href={`https://github.com/${item.user_name}`}
-                            target={"_blank"}
-                          >
-                            <img src={item.avatar_url} alt="" />
-                            <p>{item.name}</p>
-                          </a>
-                        </div>
-                        <div className="contributor-work">
-                          <ul>
-                            {item.pull_requests.map((pr, idx) => {
-                              return (
-                                <li key={idx}>
-                                  <a
-                                    href={pr.url}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                  >
-                                    #{pr.id} - {pr.title}
-                                  </a>
-                                  {` | Created On ${pr.created_on}`}
-                                </li>
-                              );
-                            })}
-                          </ul>
-                        </div>
-                      </div>
-                    );
-                })}
-            </div>
-        </Container>
-    )
-}
+  const toggleVisibility = (index) => {
+    setIsVisible((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !newState[index];
+      return newState;
+    });
+  };
 
-export default ContributorWork
+  return (
+    <Container>
+      <h1 className="main-heading">Contributor Work</h1>
+      <p className="heading-supporter">
+        We would like to extend our heartfelt gratitude to all the dedicated
+        contributors who have poured their time, effort, and expertise into our
+        project. Your unwavering commitment and valuable contributions have
+        played an instrumental role in our success. Your creativity, dedication,
+        and passion for the work you do have not only elevated our project but
+        also inspired everyone around you. We truly appreciate your hard work
+        and look forward to continuing this journey of collaboration and
+        achievement together. Thank you for your exceptional contributions; they
+        are the cornerstone of our progress.
+      </p>
+      
+      <div className="hold-contributors">
+        {contributorsPullRequests != null &&
+          contributorsPullRequests.map((item, index) => {
+            return (
+              <div key={index} className="contributor-work-container">
+                <div className="contributor-info">
+                  <a
+                    href={`https://github.com/${item.user_name}`}
+                    target={"_blank"}
+                  >
+                    <img src={item.avatar_url} alt="" />
+                  </a>
+                </div>
+                <div className="contributor-work">
+                  <h3>{item.name} <a href={`https://github.com/${item.user_name}`}><span>@{item.user_name}</span></a></h3>
+                  <p>{item.description}</p>
+                 
+                  {isVisible[index] ? (
+                    <>
+                      <a onClick={() => toggleVisibility(index)}>Show less</a>
+                      <ul className={isVisible[index] ? "show" : "hide"}>
+                        {item.pull_requests.map((pr, idx) => {
+                          return (
+                            <li key={idx}>
+                              <a
+                                href={pr.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                #{pr.id} - {pr.title}
+                              </a>
+                              {` | Created On ${pr.created_on}`}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </>
+                  ) : (
+                    <a onClick={() => toggleVisibility(index)}>Show more</a>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </Container>
+  );
+};
 
+export default ContributorWork;
 
 const Container = styled.div`
   width: 100%;
@@ -69,13 +92,14 @@ const Container = styled.div`
 
   .hold-contributors {
     margin-top: 40px;
+    padding: 10px;
 
     .contributor-work-container {
       flex-direction: row;
       display: flex;
       margin-bottom: 8px;
       padding: 16px;
-
+      border-bottom: 1px solid #e5e5e5;
       .contributor-info {
         width: 20%;
         text-align: center;
@@ -112,17 +136,50 @@ const Container = styled.div`
       .contributor-work {
         margin-left: 30px;
         width: 80%;
-
+        h3{
+          margin-bottom: 5px;
+          a{
+            text-decoration:none;
+          }
+          span{
+            font-size: 0.75rem;
+            font-weight: 400;
+            color: #374151;
+            margin-left: 5px;
+          }
+        }
+        p{
+          font-size: 0.75rem;
+        }
+        a{
+          margin:2px;
+          font-size: 0.75rem;
+          background-color: #ffff;
+          cursor: pointer;
+        }
+        .show {
+          max-height: 500px; 
+          overflow: hidden;
+          transition:  3s ease-in;
+        }
+      
+        .hide {
+          max-height: 0;
+          overflow: hidden;
+          transition: max-height 0.3s ease-in-out;
+        }
         ul {
+          margin-top: 10px;
           list-style: none;
-          font-size: 0.85rem;
+          font-size: 0.65rem;
           font-weight: 200;
           line-height: 1.5rem;
-
+         
           li {
             margin-bottom: 8px;
           }
         }
+        
       }
     }
   }
