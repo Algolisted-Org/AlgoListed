8e2a814822af81934f43a2525f819d3a855262b4
Nayaker <nayak.primary@gmail.com>
2023-10-15 03:15:16 +0530

    Opportunities Page Update (v2.10 Plan Fixed)

--- client/src/Screen/Opportunities.js
+++ client/src/Screen/Opportunities.js
@@ -5,6 +5,7 @@ import LeftMenu from '../Components/LeftMenu'
 import FilterListIcon from '@material-ui/icons/FilterList';
 import InfoIcon from '@material-ui/icons/Info';
 import SimpleFooter from "../Components/SimpleFooter";
+import CallMadeIcon from '@material-ui/icons/CallMade';
 
 const Opportunities = () => {
     useEffect(() => {
@@ -25,14 +26,45 @@ const Opportunities = () => {
                   <div className="cc-middle-content">
                       <h1 className='main-heading'>All Internship & Job Opportunities <div className="head-tag">coming soon</div> </h1>
                       <p className="heading-supporter">
-                        On this page, you'll find details about various job openings and internship opportunities, along with the corresponding schedules. We are presently in the process of planning and will be unveiling this page shortly. To have your LinkedIn post showcased on this page, simply include the hashtag <div className="special-blue">#list_as_hiring_on_algolisted</div>
+                        On this page, you'll find details about various job openings and internship opportunities, along with the corresponding schedules. We are presently in the process of planning and will be unveiling this page shortly. 
                       </p>
-                      <div className="message">
-                          <div className="icon"></div>
-                          <div className="text">
-                          Simply add the hashtag <b>#list_as_hiring_on_algolisted</b> to your LinkedIn post for it to be featured on this page.
+                      <p className="heading-supporter">
+                        We currently have two plans : 
+                      </p>
+                      <div className="messages">
+                        <div className="message">
+                            <div className="icon"></div>
+                            <div className="text">
+                              We are seeking out <b>small YouTube, Linkedin or Telegram channels</b> engaged in similar activities to ours, with a mutual interest in collaborating on this website.
+                              If interested <i><a href="mailto:nayak.primary@gmail.com">contact here</a></i>
+                            </div> 
+                        </div> 
+                        <div className="message">
+                            <div className="icon"></div>
+                            <div className="text">
+                            Make a public JSON file on GitHub that anyone can add to. You can include links to job openings at the company, and we'll display them on our website. These job listings will only be visible for one week, or a maximum of 7 days. To do this, we need people to help manage and ensure the job opportunities are valid. If you're interested in being a maintainer, please get in touch with us. <i><a href="mailto:nayak.primary@gmail.com">contact here</a></i>
+                            </div> 
+                        </div> 
+                        <div className="messages2">
+                          <h3>Have got an innovative way to resolve this issue? </h3>
+                          <div className="message2">
+                              <a href="/" className="text">
+                                Express your thoughts on github issue #145
+                              </a> 
+                              <div className="icon">
+                                <CallMadeIcon/>
+                              </div>
                           </div> 
-                      </div> 
+                          <div className="message2">
+                              <a href="/" className="text">
+                                Express your with Algolisted owner
+                              </a> 
+                              <div className="icon">
+                                <CallMadeIcon/>
+                              </div>
+                          </div> 
+                        </div>
+                      </div>
                       
                   </div>
                   <SimpleFooter></SimpleFooter>
@@ -140,24 +172,69 @@ const Container = styled.div`
           }
       }
 
-      .message{
-        display: inline-block;
-        /* display: flex; */
-        /* align-items: center; */
-        background-color: #d5f7e1;
-        border-radius: 5px;
-        padding: 10px;
-        margin: 20px 0 10px 0;
+      .messages{
+        .message{
+          display: inline-block;
+          /* display: flex; */
+          /* align-items: center; */
+          background-color: #d5f7e1;
+          border-radius: 5px;
+          padding: 10px;
+          margin: 0 0 10px 0;
+  
+          .text{
+              font-size: 0.8rem;
+              color: #13803b;
+              font-weight: 300;
+              
+              b{
+                font-weight: 400;
+              }
+  
+              a{
+                margin-left: 2.5px;
+                text-decoration: none;
+              }
+          }
+  
+  
+        } 
+      }
 
-        .text{
-            font-size: 0.8rem;
-            color: #13803b;
-            font-weight: 300;
-            
-            b{
-              font-weight: 400;
-            }
+      .messages2{
+        margin: 30px 0 10px 0;
+        h3{
+          font-size: 1.25rem;
+          font-weight: 500;
+        }
+        .message2{
+          display: flex;
+          align-items: center;
+          border-radius: 5px;
+          margin: 10px 0 0 0;
+  
+          .text{
+              font-size: 0.9rem;
+              font-weight: 300;
+              margin-right: 5px;
+              
+              b{
+                font-weight: 400;
+              }
+  
+              a{
+                margin-left: 2.5px;
+                text-decoration: none;
+              }
+          }
+  
+          svg{
+            font-size: 1rem;
+          }
+  
+  
         }
       }
+
     }
 `
