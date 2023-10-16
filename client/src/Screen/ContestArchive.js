a5f90086d5dad7a5ec7de063637337f4d0e87333
Nayaker <nayak.primary@gmail.com>
2023-10-16 15:43:29 +0530

    Create Custion Coding Sheet - [Add link fun added] - delete

--- client/src/Screen/ContestArchive.js
+++ client/src/Screen/ContestArchive.js
@@ -21,6 +21,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { FaWineBottle } from 'react-icons/fa';
 
 
 const ContestArchive = () => {
@@ -28,18 +29,21 @@ const ContestArchive = () => {
   const [contestType, setContestType] = useState('Weekly Contest');
   const [contestNumber, setContestNumber] = useState('361');
   const [needDarkMode, setNeedDarkMode] = useState(false);
-  const [showTags, setShowTags] = useLocalStorage("showTags",true);
+  const [showTags, setShowTags] = useLocalStorage("showTags", true);
   const [filterContestType, setFilterContestType] = useState("All");
   const [filterContestTypeName, setFilterContestTypeName] = useState("Both Contest Types");
   const [openModel1, setOpenModel1] = useState(false);
   const [openModel2, setOpenModel2] = useState(false);
   const [sliderInputValue, setSliderInputValue] = useState(20);
   const [filteredContestData, setFilteredContestData] = useState(contestsData);
-  const [checkbox1, setCheckbox1] = useLocalStorage("checkbox1",false);
-  const [checkbox2, setCheckbox2] = useLocalStorage("checkbox2",false);
-  const [checkbox3, setCheckbox3] = useLocalStorage("checkbox3",false);
-  const [checkbox4, setCheckbox4] = useLocalStorage("checkbox4",false);
-
+  const [checkbox1, setCheckbox1] = useLocalStorage("checkbox1", false);
+  const [checkbox2, setCheckbox2] = useLocalStorage("checkbox2", false);
+  const [checkbox3, setCheckbox3] = useLocalStorage("checkbox3", false);
+  const [checkbox4, setCheckbox4] = useLocalStorage("checkbox4", false);
+  // const [checkboxShared,setCheckboxShared] = useState(localStorage.getItem("checkboxShared") || {0:false, 1:false, 2:false, 3:false});
+  // const [checkboxShared, setCheckboxShared] = useLocalStorageCustom("checkboxShared", []);
+  const [checkboxstate,setCheckboxstate]= useState(JSON.parse(localStorage.getItem("checkboxShared")) || null)
+  let arr;
   useEffect(() => {
     let selectedTheme = localStorage.getItem("selectedTheme");
     if (selectedTheme === 'dark') setNeedDarkMode(true);
@@ -100,14 +104,75 @@ const ContestArchive = () => {
   const onClick4 = (e) => {
     setCheckbox4(e.target.checked)
   }
+
+
+    const onClickShared = (pname, cname) => {
+      pname = pname.toLowerCase().replaceAll(" ", "_");
+      cname = cname.toLowerCase().replaceAll(" ", "_");
+      let x = JSON.parse(window.localStorage.getItem("checkboxShared"));
+      if (!x) {
+        const objToSave = {
+          [cname]: {
+            [pname]: true,
+          }
+        }
+        window.localStorage.setItem("checkboxShared", JSON.stringify(objToSave));
+        setCheckboxstate(objToSave);
+        return;
+      }
+
+      if (!Object.keys(x).includes(cname)) {
+        const objToSave = {
+          ...x,
+          [cname]: {
+            [pname]: true,
+          }
+        }
+        window.localStorage.setItem("checkboxShared", JSON.stringify(objToSave));
+        // console.log(objToSave);
+        setCheckboxstate(objToSave);
+
+        return;
+      }
+
+      if (!Object.keys(x[cname]).includes(pname)) {
+        const objToSave = {
+          ...x,
+          [cname]:
+          {
+            ...x[cname],
+            [pname]: true
+          }
+        }
+        window.localStorage.setItem("checkboxShared", JSON.stringify(objToSave));
+        // console.log(objToSave);
+        setCheckboxstate(objToSave);
+
+
+        return;
+      }
+
+      const newObject = { ...x[cname] }
+      delete newObject[pname];
+
+      const objToSave = {
+        ...x,
+        [cname]: newObject
+      }
+
+      localStorage.setItem("checkboxShared", JSON.stringify(objToSave));
+      // console.log(objToSave);
+      setCheckboxstate(objToSave)
+      
+    }
+
+    const isQuestionSolved = (cname,pname)=>{
+      cname = cname.toLowerCase().replaceAll(" ","_");
+      pname = pname.toLowerCase().replaceAll(" ","_");
+      return checkboxstate && checkboxstate?.[cname]&& checkboxstate[cname]?.[pname]
+    }
   
-  useEffect(()=>{
-    window.localStorage.setItem("CheckBox1",JSON.stringify(checkbox1));
-    window.localStorage.setItem("CheckBox2",JSON.stringify(checkbox2));
-    window.localStorage.setItem("CheckBox3",JSON.stringify(checkbox3));
-    window.localStorage.setItem("CheckBox4",JSON.stringify(checkbox4));
-  },[checkbox1,checkbox2,checkbox3,checkbox4])
-  
+
   const redirectToContest = () => {
     const url = `/contest-analysis/${contestType.toLowerCase().replace(' ', '-')}-${contestNumber}`;
     window.location.href = url;
@@ -287,7 +352,7 @@ const ContestArchive = () => {
               <div className="filter-item">B</div>
               <div className="filter-item">C</div>
               <div className="filter-item">D</div> */}
-              <div className="filter-item" onClick={()=>{setShowTags(!showTags); }}>{showTags ? "Hide Problem Tags" : "Show Problem Tags"}</div>
+              <div className="filter-item" onClick={() => { setShowTags(!showTags); }}>{showTags ? "Hide Problem Tags" : "Show Problem Tags"}</div>
               {/* <div className="filter-item">Show Unsolved</div>  */}
             </div>
           </EffectiveFilter>
@@ -308,68 +373,6 @@ const ContestArchive = () => {
 
 
           <div className="problems-table">
-            <div className="one-contest-problems">
-              <div className="contest-name">Dummy Contest 1</div>
-              <div className="contest-outlinks">
-                <a target='_blank' className="link">
-                  <CallMadeIcon />
-                </a>
-                <a target='_blank' className="link">
-                  <EqualizerIcon />
-                </a>
-                <div className="link">
-                  <CreateIcon />
-                </div>
-              </div>
-              <div className={`contest-problem  ${checkbox1 && "solved-problem"}`}>
-                <div className="problem-main-name">
-                  <label>
-                    <input type="checkbox" value= {checkbox1} onChange={onClick1} checked={checkbox1}/>
-                    Problem Unsolved
-                  </label>
-                  <div className="problem-name">A. I am Dummy</div>
-                </div>
-                <div className="problem-info">
-                  <div className="tag">Problem Tags are Hidden</div>
-                </div>
-              </div>
-              <div className={`contest-problem  ${checkbox2 && "solved-problem"}`}>
-                <div className="problem-main-name">
-                  <label>
-                    <input type="checkbox" value= {checkbox2} onChange={onClick2} checked={checkbox2}/>
-                    Problem Unsolved
-                  </label>
-                  <div className="problem-name">B. I am Dummy</div>
-                </div>
-                <div className="problem-info">
-                  <div className="tag">Problem Tags are Hidden</div>
-                </div>
-              </div>
-              <div className={`contest-problem  ${checkbox3 && "solved-problem"}`}>
-                <div className="problem-main-name">
-                  <label>
-                    <input type="checkbox"value= {checkbox3} onChange={onClick3} checked={checkbox3}/>
-                    Problem Unsolved
-                  </label>
-                  <div className="problem-name">C. I am Dummy</div>
-                </div>
-                <div className="problem-info">
-                  <div className="tag">Problem Tags are Hidden</div>
-                </div>
-              </div>
-              <div className={`contest-problem  ${checkbox4 && "solved-problem"}`}>
-                <div className="problem-main-name">
-                  <label>
-                    <input type="checkbox" value= {checkbox4} onChange={onClick4} checked={checkbox4}/>
-                    Problem Unsolved
-                  </label>
-                  <div className="problem-name">D. I am Dummy</div>
-                </div>
-                <div className="problem-info">
-                  <div className="tag">Problem Tags are Hidden</div>
-                </div>
-              </div>
-            </div>
             {filteredContestData.map((contestData, index) => (
               <div className="one-contest-problems" key={index}>
                 <div className="contest-name">{contestData.contest_name}</div>
@@ -385,10 +388,10 @@ const ContestArchive = () => {
                   </div>
                 </div>
                 {Object.values(contestData.problems).map((problem, problemIndex) => (
-                  <div className="contest-problem" key={problemIndex}>
+                  <div className={`contest-problem ${isQuestionSolved(contestData.contest_name,problem.name)?"solved-problem":""}`} key={problemIndex}>
                     <div className="problem-main-name">
                       <label>
-                        <input type="checkbox" />
+                        <input type="checkbox" onChange={() => { onClickShared(problem.name, contestData.contest_name) }} checked={isQuestionSolved(contestData.contest_name,problem.name)}/>
                         Problem Unsolved
                       </label>
                       <div className="problem-name">{String.fromCharCode(65 + problemIndex)}. {problem.name}</div>
@@ -408,7 +411,6 @@ const ContestArchive = () => {
                           <div className="tag">Problem Tags are Hidden</div>
                         </div>
                       )
-
                     }
 
                   </div>
