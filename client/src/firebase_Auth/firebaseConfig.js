a86ea6ca23f1f7a6530ef33f37f87a4eba20a0ba
mit-anuj <anujmittal0040@gmail.com>
2023-10-19 16:53:18 +0530

    issue #153 resolved

---
+++ client/src/firebase_Auth/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
