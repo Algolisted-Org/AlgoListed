7c2942f7a0a9de55532f98d3045408b716cfb302
Nayaker <nayak.primary@gmail.com>
2023-11-11 09:01:02 +0530

    coding resources filters update

---
+++ client/src/Components/codingResourcesFilters.js
@@ -0,0 +1,55 @@
+
+export const codingResourcesFilters = [
+    {
+        text: "All Resources",
+        id: 1,
+    },
+    {
+        text: "Frontend",
+        id: 2,
+    },
+    {
+        text: "Backend",
+        id: 3,
+    },
+    {
+        text: "Dynamic Programming",
+        id: 4,
+    },
+    {
+        text: "Graphs",
+        id: 5,
+    },
+    {
+        text: "Trees",
+        id: 6,
+    },
+    {
+        text: "Core Subjects",
+        id: 7,
+    },
+    {
+        text: "Competitive Programming",
+        id: 8,
+    },
+    {
+        text: "Interview Prep",
+        id: 9,
+    },
+    {
+        text: "DSA",
+        id: 10,
+    },
+    {
+        text: "Machine Learning",
+        id: 11,
+    },
+    {
+        text: "Image Processing",
+        id: 12,
+    },
+    {
+        text: "NLP",
+        id: 13,
+    },
+];
