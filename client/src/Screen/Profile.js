f3f9e54d3a6a97eac316a5f2512df277e73470f9
Nayaker <nayak.primary@gmail.com>
2024-01-26 18:08:54 +0530

    profile page update

--- client/src/Screen/Profile.js
+++ client/src/Screen/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import LinkIcon from '@material-ui/icons/Link';
 
 const Profile = () => {
     return (
@@ -16,26 +17,35 @@ const Profile = () => {
                     Presently, I am in my third year pursuing computer science at Jadavpur University in Kolkata, India. My interests lie in constructing scalable cloud architectures, developing scalable websites, and implementing time-optimized algorithms. Over the recent months, I have delved into the realms of machine learning and cloud architecture. I pride myself on being a proficient problem solver, holding the title of an Expert on Codeforces and achieving the status of a knight on LeetCode.
                 </p>
                 <div className="tags">
-                    <VerifiedUserIcon/>
+                    <VerifiedUserIcon />
                     <div className="tag">Web Development</div>
                     <div className="tag">Cloud Architecture</div>
                     <div className="tag">Machine Learning</div>
                     <div className="tag">Problem Solving Expert</div>
                 </div>
             </div>
+            <div className="table-of-contents">
+                <h2>Table of Contents</h2>
+                <div className="links">
+                    <a href="/"><LinkIcon/> Work Experience</a>
+                    <a href="/"><LinkIcon/> NOTABLE PROJECTS</a>
+                    <a href="/"><LinkIcon/> Open Source Contributions</a>
+                    <a href="/"><LinkIcon/> Achievements</a>
+                </div>
+            </div>
             <div className="section">
-                <h2>Notable Projects</h2>
+                <h2><LinkIcon /> Notable Projects</h2>
 
                 <div className="project">
-                    <h3>1. algolisted.com 
-                        <span>Web Application</span> 
-                        <span>Open Sourced</span> 
+                    <h3>1. algolisted.com
+                        <span>Web Application</span>
+                        <span>Open Sourced</span>
                     </h3>
                     <p className="desc">
-                    Algolisted is a platform dedicated to analyzing contests on platforms like LeetCode and Codeforces. It offers a range of features, including trackers and visualization charts, to enhance learning productivity. Notably, we have introduced a resume-based question generator powered by <b>OpenAI</b>, allowing users to receive interview questions tailored to their resumes. This innovative tool facilitates effective interview preparation.
+                        Algolisted is a platform dedicated to analyzing contests on platforms like LeetCode and Codeforces. It offers a range of features, including trackers and visualization charts, to enhance learning productivity. Notably, we have introduced a resume-based question generator powered by <b>OpenAI</b>, allowing users to receive interview questions tailored to their resumes. This innovative tool facilitates effective interview preparation.
                     </p>
                     <p className="desc">
-                    The primary technology stack employed in constructing the website is <b>MERN</b>. <b>Python</b> and <b>Flask</b> were utilized for scraping and processing data. The deployment was executed through <b>Google Firebase</b>, <b>cyclic.sh</b>, and <b>Vercel</b>.
+                        The primary technology stack employed in constructing the website is <b>MERN</b>. <b>Python</b> and <b>Flask</b> were utilized for scraping and processing data. The deployment was executed through <b>Google Firebase</b>, <b>cyclic.sh</b>, and <b>Vercel</b>.
                     </p>
                     {/* <p className="desc">
                         <b className='highlight'>Data Encryption</b> : To safeguard API data, I implemented a custom <b>npm package</b> called <b>easy-api-encryptor</b>. The encryption algorithm is elucidated in the subsequent section of the projects.
@@ -50,8 +60,8 @@ const Profile = () => {
 
                 <div className="project">
                     <h3>2. Slurp Software
-                        <span>Web Application</span> 
-                        <span>Profitable Software</span> 
+                        <span>Web Application</span>
+                        <span>Profitable Software</span>
                     </h3>
                     <p className="desc">
                         "Slurp" is currently in the development phase, essentially designed as a restaurant management system incorporating QR code functionality and real-time updates.
@@ -63,8 +73,8 @@ const Profile = () => {
 
                 <div className="project">
                     <h3>3. Vision Bridge [ Hackathon Winning Project ]
-                        <span>Machine Learning</span> 
-                        <span>Web Simulator</span> 
+                        <span>Machine Learning</span>
+                        <span>Web Simulator</span>
                     </h3>
                     <p className="desc">
                         Engineered software for visually impaired users, featuring ML models with over 85% accuracy. Includes currency recognition, traffic identification, obstacle detection, emotion analysis, and text scanning. Voice input and automatic ML model selection enhance user experience. Technologies: React, Flask, TensorFlow, OpenCV.
@@ -75,16 +85,28 @@ const Profile = () => {
                 </div>
 
                 <div className="project">
-                    <h3>4. Leetcode Friends Live Contest Ranking 
-                        <span>Chrome Extension</span> 
+                    <h3>4. Leetcode Friends Live Contest Ranking
+                        <span>Chrome Extension</span>
                     </h3>
                     <p className="desc">
-                        Engineered software for visually impaired users, featuring ML models with over 85% accuracy. Includes currency recognition, traffic identification, obstacle detection, emotion analysis, and text scanning. Voice input and automatic ML model selection enhance user experience. Technologies: React, Flask, TensorFlow, OpenCV.
-                    </p>
-                    <p className="desc">
-                        Utilizes React for frontend, Flask for server, and TensorFlow/OpenCV for ML. Ensures seamless integration of voice input and ML capabilities for optimal accessibility.
+                        The Chrome extension, "Leetcode Friends Live Contest Ranking," enables users to view the rankings of their friends in a LeetCode contest. This extension fulfills a high demand as it addresses a missing feature on the LeetCode website during contests.
                     </p>
                 </div>
+
+                <div className="project">
+                    <h3>5. easy-api-encryptor
+                        <span>NPM Package</span>
+                    </h3>
+                    <p className="desc">
+                        <b className='highlight'>NPM Package</b> : "easy-api-encryptor" is a lightweight package facilitating seamless encryption and decryption of data between Node.js and React.js. Ensure secure communication in your applications with minimal setup, making it easy to protect sensitive information during transit.
+                    </p>
+                    <p className="desc">
+                        <b className='highlight'>Encryption Algorithm</b> : The encryption model employed here involves a simple yet effective technique of <b>adding a cyclic value of 15 to each character in the input string</b>. This process applies uniformly to both lowercase and uppercase characters, creating a straightforward and <b>reversible encryption mechanism</b>. The cyclic nature ensures that the shift wraps around, allowing for a seamless transformation while maintaining the integrity of the original character set. While this encryption model may not provide advanced security, it serves as a illustrative example for educational purposes or basic data obfuscation.
+                    </p>
+                </div>
+            </div>
+            <div className="section">
+                <h2><LinkIcon /> Open Source Contributions</h2>
             </div>
         </Container>
     )
@@ -110,7 +132,7 @@ const Container = styled.div`
         }
 
         h1{
-            font-size: 4rem;
+            font-size: 5rem;
             font-weight: 700;
             text-transform: uppercase;
             letter-spacing: 0.5rem;
@@ -160,6 +182,47 @@ const Container = styled.div`
             }
         }
     }
+
+    .table-of-contents{
+        margin-top: 60px;
+        margin-bottom: -60px;
+
+        h2{
+            font-size: 1.25rem;
+            font-weight: 600;
+            /* text-transform: uppercase; */
+            letter-spacing: 0.05rem;
+            /* color: #85c4eb; */
+
+            svg{
+                font-size: 2rem;
+            }
+        }
+
+        .links{
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            padding-left: 15px;
+            /* margin-top: 10px; */
+
+            a{
+                display: flex;
+                align-items: center;
+                text-decoration: none;
+                margin-top: 20px;
+                text-transform: uppercase;
+                /* color: cornflowerblue; */
+                font-size: 0.85rem;
+                font-weight: 500;
+                letter-spacing: 0.1rem;
+
+                svg{
+                    margin-right: 10px;
+                }
+            }
+        }
+    }
     
     .section{
         margin-top: 120px;
@@ -170,6 +233,11 @@ const Container = styled.div`
             text-transform: uppercase;
             letter-spacing: 0.15rem;
             margin-bottom: -15px;
+            /* color: #85c4eb; */
+
+            svg{
+                font-size: 2rem;
+            }
         }
 
         .project{
